64e0afe890e2cd2d413019d32a0fc2b42d8e9841
Tecco <glaypppgod56d@gmail.com>
2015-10-08 02:29:41 +0900

    てすとup

---
+++ CodeStudy/app/src/androidTest/java/com/winter_time_studio/codestudy/ApplicationTest.java
@@ -0,0 +1,43 @@
+package com.winter_time_studio.codestudy;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    private MainActivity activity;
+
+    public ApplicationTest(){
+        super(MainActivity.class);
+    }
+
+    @Before
+    public void setUp() throws Exception{
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        this.activity = getActivity();
+    }
+
+    @After
+    public void tearDown() throws Exception{
+        super.tearDown();
+    }
+
+    @Test
+    public void checkPrecondition() {
+        Espresso.onView(ViewMatchers.withId(R.id.textView1)).check(ViewAssertions.matches(ViewMatchers.withText("Hello world!2")));
+    }
+}
